4e6aaf9210272130e3cef637d592549af1769850
Gyosic <zpzptls@naver.com>
2024-12-22 03:29:50 +0900

    user get res 수정

--- app/api/users/[code]/route.ts
+++ app/api/users/[code]/route.ts
@@ -7,11 +7,14 @@ type Params = {
 export async function GET(req: NextRequest, { params }: { params: Params }) {
   const { code } = params
   try {
-    const { body: { hits: { hits } = {} } = {} } = await os.client.search({
+    const { body: { hits: { hits = [] } = {} } = {} } = await os.client.search({
       index: os.index,
       body: { query: { bool: { filter: [{ term: { 'docType.keyword': 'user' } }, { term: { 'code.keyword': code } }] } } },
     })
-    return NextResponse.json({ hits })
+    const { _source = {} } = hits?.[0] || {}
+    const user = _source
+
+    return NextResponse.json({ user })
   } catch (err: any) {
     throw new Error(err)
   }
