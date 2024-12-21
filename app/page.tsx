c53f5bd198fb3b176ac6b1c7c43a0ccfcec7be4b
최수빈 <joo00206@gractor.com>
2024-12-22 02:33:59 +0900

    fix : 운세보기 페이지 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,19 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col row-start-2 items-center sm:items-start">
-        <span className="text-lg font-bold">수빈님</span>
-        <span className="text-lg font-bold">오늘도 럭키비키잖아? </span>
+    <div className="h-full flex flex-col content-center items-center justify-center">
+        <span className="text-2xl font-bold">수빈님</span>
+        <span className="text-2xl font-bold">오늘도 럭키비키잖아? </span>
         <Image
             className="dark:invert"
             src="/giphy.gif"
             alt="giphy"
-            width={100}
-            height={100}
+            width={300}
+            height={300}
           />
-
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          오늘의 운세보기
-        </a>
-      </footer>
+       <Button className="px-20 h-14 text-base">오늘의 운세보기</Button>
     </div>
   );
 }
