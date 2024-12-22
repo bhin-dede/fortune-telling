92f84589ad4a5c8a142818c871c0caaed7733087
최수빈 <joo00206@gractor.com>
2024-12-22 20:31:32 +0900

    fix : style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
 
 export default function Home() {
@@ -8,16 +8,12 @@ export default function Home() {
 
   return (
     <div className="h-full flex flex-col content-center items-center justify-center">
-        <span className="text-2xl font-bold">수빈님</span>
-        <span className="text-2xl font-bold">완전 럭키비키잖아? </span>
-        <Image
-            className="dark:invert"
-            src="/giphy.gif"
-            alt="giphy"
-            width={300}
-            height={300}
-          />
-       <Button onClick={() => router.push(`/result`)} className="px-20 h-14 text-base">오늘의 운세보기</Button>
+      <span className="text-2xl font-bold">수빈님</span>
+      <span className="text-2xl font-bold">완전 럭키비키잖아? </span>
+      <Image className="dark:invert" src="/giphy.gif" alt="giphy" width={300} height={300} />
+      <Button onClick={() => router.push(`/result`)} className="px-20 h-14 text-base absolute bottom-10 w-5/6">
+        오늘의 운세보기
+      </Button>
     </div>
-  );
+  )
 }
