545ebff1db1f5e2ec80904d8454debc4f5c2c6c0
gyosic <gyosic@gractor.com>
2024-12-24 11:41:52 +0900

    ai model 변경

--- lib/fortuneai.ts
+++ lib/fortuneai.ts
@@ -25,7 +25,8 @@ class Fortuneai {
       timeZone: 'Asia/Seoul',
     }) //한국의 서울을 시간을 기준으로 Date 생성
     const completion = await this.openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
+      //model: 'gpt-3.5-turbo',
+      model: 'gpt-4o-mini-2024-07-18',
       max_tokens: 500, // 답변 문자열 길이, 토큰 기준(한글은 한글자에 3토큰정도, 영어는 한 토큰에 3글자정도)
       temperature: 0.9, // 랜덤한 답변 정도, 낮으면 항상 같은 답변이 제공됨 / 모델이 얼마나 Risk(=randomness)를 감수할 것인지 조절/0.9를 설정하면 creative answer, 0이면 well-defined answer
       top_p: 0.9, // 답변의 다양성 높을수록 다양한 답변이 나오는 대신, 올바르지 않은 답변이 가능함 /  token의 확률이 top_p(0.3이면 상위 30%)만큼인 토큰만 사용
