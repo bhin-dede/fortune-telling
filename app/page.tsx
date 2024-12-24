1823587bf31a2721caa470b997c54f1406a60f8c
joo00206 <joo00206@gractor.com>
2024-12-24 11:09:28 +0900

    fix: text 수정

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="h-full flex flex-col content-center items-center justify-center">
       <span className="text-2xl font-bold">오늘의 운세</span>
-      <span className="text-2xl font-bold">태그하고 안들어 와쨔냐?</span>
+      <span className="text-2xl font-bold">태그하고 안하고 그냥 들어와쨔냐?</span>
       <Image className="dark:invert" src="/giphy.gif" alt="giphy" width={300} height={300} />
       <Button disabled className="px-20 h-14 text-base absolute bottom-10 w-5/6">
         태그하고 들어오쇼
