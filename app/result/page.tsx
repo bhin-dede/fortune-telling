2111eedef90321702318da5d7627cf494117fe10
joo00206 <joo00206@gractor.com>
2024-12-24 15:35:52 +0900

    fix: text 수정

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -14,11 +14,12 @@ export default function ResultPage() {
   return (
     <>
       <main className="h-full flex flex-col content-center items-center justify-center">
-        <span className="text-neutral-600">{date}</span>
-        <span className="text-xl font-bold mb-5 w-5/6 text-center">{user.fortune?.summary}</span>
+        <span className="text-xl font-bold w-5/6 text-center">{user.name}의 오늘 운세</span>
+        <span className="text-neutral-600 mb-5">{date}</span>
+        <span className="text-md font-bold mb-5 w-5/6 text-center">🔮 {user.fortune?.summary}</span>
         <p className="w-5/6">{user.fortune?.tell}</p>
         <div className="bg-white w-5/6 mt-5 rounded-md p-2">
-          <p className="font-bold text-lime-700">오늘의 색 : {user.fortune?.colors}</p>
+          <div className="font-bold text-lime-700"> 오늘의 색 : {user.fortune?.colors} </div>
           <p className="font-bold text-lime-700">오늘의 숫자 : {user.fortune?.numbers}</p>
         </div>
         <Button onClick={() => router.push(`/${user.code}`)} className="px-20 h-14 text-base absolute bottom-10 w-5/6">
