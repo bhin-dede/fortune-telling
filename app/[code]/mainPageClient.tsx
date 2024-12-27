ad542a39febee3110a002282556be422448be86e
joo00206 <joo00206@gractor.com>
2024-12-27 10:56:14 +0900

    warning : add priority

--- app/[code]/mainPageClient.tsx
+++ app/[code]/mainPageClient.tsx
@@ -34,7 +34,7 @@ export function MainPageClient({ code }: { code: string }) {
     <>
       <span className="text-2xl font-bold">오늘 운세</span>
       <span className="text-2xl font-bold">완전 럭키{user.name}잖아? </span>
-      <Image className="dark:invert" src={src} onError={handleError} alt="럭키운세" width={300} height={300} />
+      <Image className="dark:invert" src={src} onError={handleError} alt="럭키운세" width={300} height={300} priority />
       <Button
         onClick={() => (Object.keys(user).length ? router.push(`/${code}/result`) : router.push(`/${code}/userInfo`))}
         className="px-20 h-14 text-base absolute bottom-10 w-5/6"
