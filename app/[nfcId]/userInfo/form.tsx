d19167c66465ff38c1c826fa20036a9338be9f3a
Gyosic <zpzptls@naver.com>
2025-01-04 01:08:56 +0900

    loading 추가

--- app/[nfcId]/userInfo/form.tsx
+++ app/[nfcId]/userInfo/form.tsx
@@ -10,6 +10,7 @@ import { Button } from '@/components/ui/button'
 import { useAtom } from 'jotai'
 import { responseUser } from '@/store/state'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
 export type UserFormData = z.infer<typeof UserSchema>
 const UserForm = ({ nfcId }: { nfcId: string }) => {
@@ -48,63 +49,72 @@ const UserForm = ({ nfcId }: { nfcId: string }) => {
     const onSubmit = handleSubmit(onValid, onInvalid)
     return (
       <div className="h-full flex flex-col content-center items-center justify-center">
-        <span className="text-xl font-bold">정확한 운세를 위해 정보를 입력해주세요.</span>
-        <span className="text-neutral-600 mb-5">* 운세보기 외 정보를 사용하지 않습니다.</span>
-        <Form {...form}>
-          <form className="w-5/6 flex flex-col gap-2" onSubmit={onSubmit}>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input className="h-14 bg-white" placeholder="이름" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="birth"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input className="h-14 bg-white" placeholder="19940101" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="birthTime"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input className="h-14 bg-white" placeholder="태어난시간" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="gender"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input className="h-14 bg-white" placeholder="성별" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button disabled={loading} type="submit" className="px-20 h-14 text-base absolute bottom-10 w-5/6">
-              {loading ? '잠시만 기다려주세요..' : '저장하기'}
-            </Button>
-          </form>
-        </Form>
+        {loading ? (
+          <>
+            <Image className="dark:invert" src="/fortune-cookie.gif" alt="fortune-cookie" width={300} height={300} priority />
+            <span className="text-xl font-bold mt-10">잠시만 기다려주세요..</span>
+          </>
+        ) : (
+          <>
+            <span className="text-xl font-bold">정확한 운세를 위해 정보를 입력해주세요.</span>
+            <span className="text-neutral-600 mb-5">* 운세보기 외 정보를 사용하지 않습니다.</span>
+            <Form {...form}>
+              <form className="w-5/6 flex flex-col gap-2" onSubmit={onSubmit}>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input className="h-14 bg-white" placeholder="이름" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="birth"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input className="h-14 bg-white" placeholder="19940101" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="birthTime"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input className="h-14 bg-white" placeholder="태어난시간" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="gender"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input className="h-14 bg-white" placeholder="성별" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button disabled={loading} type="submit" className="px-20 h-14 text-base absolute bottom-10 w-5/6">
+                  저장하기
+                </Button>
+              </form>
+            </Form>
+          </>
+        )}
       </div>
     )
   } else {
@@ -131,69 +141,78 @@ const UserForm = ({ nfcId }: { nfcId: string }) => {
     const onSubmit = handleSubmit(onValid, onInvalid)
     return (
       <div className="h-full flex flex-col content-center items-center justify-center">
-        <span className="text-xl font-bold">변경할 정보를 입력해주세요.</span>
-        <span className="text-neutral-600 mb-5">* 다음날 부터 변경된 정보로 운세를 볼 수 있습니다.</span>
+        {loading ? (
+          <>
+            <Image className="dark:invert" src="/fortune-cookie.gif" alt="fortune-cookie" width={300} height={300} priority />
+            <span className="text-xl font-bold mt-10">잠시만 기다려주세요..</span>
+          </>
+        ) : (
+          <>
+            <span className="text-xl font-bold">변경할 정보를 입력해주세요.</span>
+            <span className="text-neutral-600 mb-5">* 다음날 부터 변경된 정보로 운세를 볼 수 있습니다.</span>
 
-        <Form {...form}>
-          <form className="w-5/6 flex flex-col gap-2" onSubmit={onSubmit}>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input className="h-14 bg-white" placeholder="이름" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="birth"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input className="h-14 bg-white" placeholder="19940101" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="birthTime"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input className="h-14 bg-white" placeholder="태어난시간" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="gender"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input className="h-14 bg-white" placeholder="성별" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex w-full gap-2">
-              <Button disabled={loading} type="button" variant="outline" className="flex-1 h-14" onClick={() => router.push(`/${nfcId}`)}>
-                취소
-              </Button>
-              <Button disabled={loading} type="submit" className="text-base flex-1 h-14">
-                {loading ? '잠시만 기다려주세요..' : '저장하기'}
-              </Button>
-            </div>
-          </form>
-        </Form>
+            <Form {...form}>
+              <form className="w-5/6 flex flex-col gap-2" onSubmit={onSubmit}>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input className="h-14 bg-white" placeholder="이름" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="birth"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input className="h-14 bg-white" placeholder="19940101" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="birthTime"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input className="h-14 bg-white" placeholder="태어난시간" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="gender"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input className="h-14 bg-white" placeholder="성별" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex w-full gap-2">
+                  <Button disabled={loading} type="button" variant="outline" className="flex-1 h-14" onClick={() => router.push(`/${nfcId}`)}>
+                    취소
+                  </Button>
+                  <Button disabled={loading} type="submit" className="text-base flex-1 h-14">
+                    저장하기
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          </>
+        )}
       </div>
     )
   }
